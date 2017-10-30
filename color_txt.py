fd444b5f4202c96341af50d88a7b7d57cb6ce1f5
Gideon C <31545018+Jerubaal42@users.noreply.github.com>
2017-10-30 08:39:23 -0700

    Add files via upload

--- color_txt.py
+++ color_txt.py
@@ -1,1 +1,11 @@
-print ("\033[0;30;42mWHEE\033[0;39;49m")
+for x in range(40,48):
+	for i in range(30,38):
+		print ("\033[0;{0};{1}mCOLOR".format(i,x))
+	for i in range(90,98):
+		print ("\033[0;{0};{1}mCOLOR".format(i,x))
+for x in range(100,108):
+	for i in range(30,38):
+		print ("\033[0;{0};{1}mCOLOR".format(i,x))
+	for i in range(90,98):
+		print ("\033[0;{0};{1}mCOLOR".format(i,x))
+print("\033[0;97;40mDone")
